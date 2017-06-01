854a22230745ea68b2e11e116089bfadefe7eabb
Joao Nassar Galante Guedes <joaovngg@gmail.com>
2017-06-01 16:19:18 -0300

    First Commit

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { MelhorMedicoPage } from './app.po';
-
-describe('melhor-medico App', function() {
-  let page: MelhorMedicoPage;
-
-  beforeEach(() => {
-    page = new MelhorMedicoPage();
-  });
-
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
-  });
-});
+// import { MelhorMedicoPage } from './app.po';
+//
+// describe('melhor-medico App', function() {
+//   let page: MelhorMedicoPage;
+//
+//   beforeEach(() => {
+//     page = new MelhorMedicoPage();
+//   });
+//
+//   it('should display message saying app works', () => {
+//     page.navigateTo();
+//     expect(page.getParagraphText()).toEqual('app works!');
+//   });
+// });
